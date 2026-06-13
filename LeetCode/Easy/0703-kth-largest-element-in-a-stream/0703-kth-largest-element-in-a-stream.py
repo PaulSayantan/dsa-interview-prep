a76f51176aabf60a8ab8256027d7b788338f6c6b
Sayantan Paul <belikesayantan12@gmail.com>
2026-06-13 11:36:57 +0530

    Time: 13 ms (76.61%) , Space: 25.5 MB (58.42%) , Solved On: 06-13-2026
    - Problem: 0703-kth-largest-element-in-a-stream
    - Topic: Tree
    - Level:  Easy
    - Language: .py

---
+++ LeetCode/Easy/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -0,0 +1,27 @@
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+
+        # create a min-heap
+        heapq.heapify(self.heap)
+
+        # remove elements untill heap size reduces to k
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+        return self.heap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
